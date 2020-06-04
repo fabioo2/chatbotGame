e1806fd997ecfef63018f1ae6cad83b5a3b4af78
fabioo2 <fabio.kim2@gmail.com>
2020-06-04 14:03:46 -0400

    initial commit

--- Game.js
+++ Game.js
@@ -1,6 +1,13 @@
 module.exports = class Game {
+  constructor() {
+    this.nCurrent = 0;
+  }
   makeAMove(sInput) {
-    let aReturn = ['I know you are but what am I', 'So there'];
+    let aReturn = [];
+    if (this.nCurrent === 0) {
+      aReturn.push('Welcome to the Game 21');
+    }
+    aReturn = ['I know you are but what am I'];
     return aReturn;
   }
 };
